11f252f227f120ed7df6f770d01423cf07510ece
unknown <ruicosta607@gmail.com>
2022-03-01 17:09:17 +0000

    Base Code (First Page & Profile Page)

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonAvatar, IonChip, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { eye, eyeOff } from 'ionicons/icons';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -7,16 +8,39 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Occurrences</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Occurrences</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
+        <IonList>       
+           <IonItem>
+          <IonAvatar slot="start">
+              <img src="https://static.remove.bg/remove-bg-web/59c96072ccf69a79c0e6dd85a2eac05ceb4d0784/assets/start_remove-c851bdf8d3127a24e2d137a55b1b427378cd17385b01aec6e59d5d4b5f39d2ec.png"/>
+          </IonAvatar>
+          <IonLabel>
+            <h2>Occurence</h2>
+            <p>Title: Titulo</p>
+          </IonLabel>
+          <IonChip slot="end"><IonIcon icon={eye} /></IonChip>
+        </IonItem>
+        <IonItem>
+          <IonAvatar slot="start">
+              <img src="https://d5nunyagcicgy.cloudfront.net/external_assets/hero_examples/hair_beach_v391182663/result.jpeg"/>
+          </IonAvatar>
+          <IonLabel>
+            <h2>Occurence 2</h2>
+            <p>Title: Titulo 2</p>
+          </IonLabel>
+          <IonChip slot="end"><IonIcon icon={eyeOff} /></IonChip>
+        </IonItem>
+        </IonList>
+
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,4 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonPage, IonRow, IonText, IonTitle, IonToolbar } from '@ionic/react';
+import { arrowBackOutline, arrowForward, bookmarkOutline, chatboxEllipsesOutline, ellipsisHorizontal, imageOutline, personAddOutline } from 'ionicons/icons';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 
@@ -6,17 +7,104 @@ const Tab3: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+      <IonToolbar>
+      <IonToolbar>
+          <IonTitle>Profile</IonTitle>
         </IonToolbar>
+
+					<IonButtons slot="end">
+
+						<IonButton color="dark">
+							<IonIcon icon={ ellipsisHorizontal } />
+						</IonButton>
+					</IonButtons>
+				</IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <IonGrid>
+        <IonRow className="ion-justify-content-center">
+						<IonCol size="12" className="ion-justify-content-center ion-align-items-center ion-text-center">
+							<IonCard >
+								<IonCardContent>
+									<IonRow>
+										<IonCol size="4">
+											<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgh-NtpTM0ZaPsRftxR-JAn6hAaOuXKW7nywASWq2OT8uS1XtmA8ttGtvGhxISFJpRrqA&usqp=CAU" alt="avatar"/>
+										</IonCol>
+										<IonCol size="8">
+											<IonRow>
+												<IonCol size="12">
+													<IonText color="dark" >
+														<p>Rui Faria</p>
+													</IonText>
+													<IonText color="medium">
+														<p>Minister of Pools</p>
+													</IonText>
+												</IonCol>
+											</IonRow>
+                      <IonButton color="primary" expand="block" fill="outline">
+												<IonIcon icon={ personAddOutline } size="small" />&nbsp;
+												Edit
+											</IonButton>
+										</IonCol>
+									</IonRow>
+								</IonCardContent>
+							</IonCard>
+						</IonCol>
+					</IonRow>
+
+					<IonRow >
+						<IonCol size="12">
+							<IonCard >
+								<IonCardHeader>
+									<IonRow >
+										<IonIcon />
+										<IonCardTitle>Description</IonCardTitle>
+									</IonRow>
+								</IonCardHeader>
+								<IonCardContent>
+									<IonText>
+										<p>The minister of pools is the one person that ministrates pools!</p>
+									</IonText>
+								</IonCardContent>
+							</IonCard>
+						</IonCol>
+					</IonRow>
+
+					<IonRow >
+						<IonCol size="6">
+							<IonCard >
+								<IonCardContent>
+									<IonIcon icon={ imageOutline } />
+									<IonCardTitle>147</IonCardTitle>
+									<IonCardSubtitle>Occurences</IonCardSubtitle>
+								</IonCardContent>
+							</IonCard>
+						</IonCol>
+
+						<IonCol size="6">
+							<IonCard >
+								<IonCardContent>
+									<IonIcon icon={ bookmarkOutline } />
+									<IonCardTitle>63</IonCardTitle>
+									<IonCardSubtitle>Responded</IonCardSubtitle>
+								</IonCardContent>
+							</IonCard>
+						</IonCol>
+					</IonRow>
+
+					<IonRow >
+						<IonCol size="12">
+							<IonCard >
+								<IonCardContent>
+									<IonRow className="ion-justify-content-between">
+										<IonCardSubtitle>View latest occurences reported</IonCardSubtitle>
+										<IonIcon icon={ arrowForward } />
+									</IonRow>
+								</IonCardContent>
+							</IonCard>
+						</IonCol>
+					</IonRow>
+          </IonGrid>
       </IonContent>
     </IonPage>
   );
